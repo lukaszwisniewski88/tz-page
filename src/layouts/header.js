ce9e9f59337f51004386500f9d7640725d08456c
lukaszwisniewski88 <fr.luksus@gmail.com>
2021-04-26 19:00:15 +0200

    menu layout fix.

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -7,11 +7,11 @@ const takeUp = css`
   ${tw`z-10`}
 `
 const wrapperStyle = css`
-  ${tw`flex flex-col items-center relative p-4`}
+  ${tw`flex flex-col items-center relative p-4 gap-4`}
 `
 
 const MenuButton = styled.a(({ primary }) => [
-  tw`text-black font-extrabold uppercase bg-gray-300 p-2 rounded w-full hover:bg-opacity-80`,
+  tw`text-black font-extrabold uppercase bg-gray-300 p-4 text-center rounded w-full hover:bg-opacity-80`,
   primary && tw`bg-gray-50`,
 ])
 const Header = () => (
@@ -20,7 +20,7 @@ const Header = () => (
       alt="background"
       css={tw`absolute top-0 left-0 w-full h-full z-0`}
       objectFit="cover"
-      objectPosition="50% 0%"
+      objectPosition="50% 10%"
       quality="90"
       src="../images/bg_hero.png"
     />
@@ -33,7 +33,7 @@ const Header = () => (
     <p tw=" invisible md:visible text-xl font-extralight z-10 text-white font-serif">
       Twoja przesyłka w dobrych rękach
     </p>
-    <nav tw="flex flex-col space-y-4 justify-center items-center z-10 text-center p-4">
+    <nav tw="flex flex-col lg:flex-row z-10 p-4 gap-4">
       <MenuButton href="#Flota">flota</MenuButton>
       <MenuButton href="#Projekty">projekty</MenuButton>
       <MenuButton href="#O nas">o nas</MenuButton>
